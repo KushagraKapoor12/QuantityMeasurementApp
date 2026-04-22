3e2fc8e449fbd45abc335e13e8bd552e342407aa
Kushagra Kapoor <kushagakapoor@gmail.com>
2026-04-22 09:03:10 +0530

    UC6: Addition of two length units

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,19 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        System.out.println(
-                QuantityLength.convert(1.0, LengthUnit.FEET, LengthUnit.INCH)
-        );
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
 
-        System.out.println(
-                QuantityLength.convert(3.0, LengthUnit.YARD, LengthUnit.FEET)
-        );
+        System.out.println(q1.add(q2)); // 2 feet
 
-        System.out.println(
-                QuantityLength.convert(36.0, LengthUnit.INCH, LengthUnit.YARD)
-        );
+        QuantityLength q3 = new QuantityLength(12.0, LengthUnit.INCH);
+        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.FEET);
 
-        System.out.println(
-                QuantityLength.convert(1.0, LengthUnit.CENTIMETER, LengthUnit.INCH)
-        );
+        System.out.println(q3.add(q4)); // 24 inches
+
+        QuantityLength q5 = new QuantityLength(1.0, LengthUnit.YARD);
+        QuantityLength q6 = new QuantityLength(3.0, LengthUnit.FEET);
+
+        System.out.println(q5.add(q6)); // 2 yards
     }
 }
